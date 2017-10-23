6d1caee832e4f7f76668be0ae1513d47854cf6bb
Corentin Molitor <corentin.molitor@gmail.com>
2017-10-23 12:27:00 +0100

    Added 'range' option to select start,stop,tep of TPM filtering

--- plot_TPM_values.py
+++ plot_TPM_values.py
@@ -8,18 +8,18 @@
 import os
 import getopt
 import sys
+import numpy as np
 from subprocess import call, Popen, PIPE
-#import numpy as np
 import matplotlib.pyplot as plt
 
 
 def usage():
-	print "-h / --help for help\nInput:\n	-i / --rsemDir for rsem directory\noutput:\n	-o / --outputDir for output directory\n"
+	print "-h / --help for help\nInput:\n	-i / --rsemDir for rsem directory\n	-r / --range for filtering range (format: start,stop,step) default:0,6,0.5\noutput:\n	-o / --outputDir for output directory\n"
 
 
 ################################## GET OPTS ##################################################
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["help", "rsemDir=", "outputDir="])
+	opts, args = getopt.getopt(sys.argv[1:], "hi:r:o:", ["help", "rsemDir=", "range=", "outputDir="])
 except getopt.GetoptError as err:
         # print help information and exit:
         print str(err)  # will print something like "option -a not recognized"
@@ -28,14 +28,22 @@ except getopt.GetoptError as err:
 
 # If geneResult == T => considers genes.results in RSEM, or else isoforms.results
 geneResult = "F"
-output = None
+outDir = None
 rsemDir = None
+start = 0
+stop = 6
+step = 0.5
 
 for o, a in opts:
 	if o in ("-i", "--rsemDir"):
 		rsemDir = a
+	elif o in ("-r", "--range"):
+		start,stop,step = a.split(",")
+		start = float(start)
+		stop = float(stop)
+		step = float(step)
 	elif o in ("-o", "--outputDir"):
-		output = a
+		outDir = a
 	elif o in ("-h", "--help"):
 		usage()
 		sys.exit()
@@ -47,7 +55,7 @@ if(rsemDir == None):
 	print "Please provide rsem directory as input (option -i / --rsemDir)"
 	sys.exit()
 
-if(output == None):
+if(outDir == None):
 	print "Please provide output directory (option -o / --outputDir)"
         sys.exit()
 
@@ -57,6 +65,19 @@ cmd = "find " + rsemDir + " -name '*.isoforms.results'"
 samples = Popen(cmd, shell=True, stdout=PIPE).communicate()[0].split("\n")
 if(samples == None):
 	print "Could not find rsem results file in folder\n"
+	sys.exit()
+
+# Double check for range
+if not(isinstance(start,(int, long, float))):
+	print("Check your range, 'start' is not an int, long or float !\n")
+        sys.exit()
+
+if not(isinstance(stop, (int, long, float))):
+	print("Check your range, 'stop' is not an int, long or float !\n")
+        sys.exit()
+
+if not(isinstance(step, (int, long, float))):
+	print("Check your range, 'step' is not an int, long or float !\n")
 	sys.exit()
 
 
@@ -70,7 +91,7 @@ for s in samples:
 		plt.figure(figsize=(20,10))
 		print(s)
 		#For each TPM threshld
-		for tpmThreshold in range(0,6):
+		for tpmThreshold in np.arange(start, stop, step):
 			# Command to filter the TPM column (the 6th one) bu threshold and count the number of lines
 			cmd = "cat " + s + " | awk '$6 >= "+str(tpmThreshold)+" { print $6 }' | wc -l"
 
@@ -90,6 +111,6 @@ for s in samples:
 		plt.title(os.path.basename(s))
 
 		#plt.show()
-		graphDir = rsemDir + "graphs/Nb_isoforms_TPM_"+ os.path.basename(s)
-		print "Saving graph into" + graphDir
-		plt.savefig(graphDir +".png", bbox_inches='tight')
+		graphFile = outDir + "/Nb_isoforms_TPM_"+ os.path.basename(s)
+		print "Saving graph :" + graphFile + "\n"
+		plt.savefig(graphFile +".png", bbox_inches='tight')
